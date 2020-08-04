882c423598c1f5734468cd9ab25c34de93fbbeea
Sangin Kim <tkddls0322@naver.com>
2020-08-04 11:04:01 +0900

    first commit

---
+++ javaEx/src/com/javaex/basic/Helloworld.java
@@ -0,0 +1,16 @@
+package com.javaex.basic;
+
+/*
+ * 화면에 Hello, World라고\ 출력 해주자
+ */
+
+// 클래스 블록
+public class Helloworld { 
+	
+	// 직접 실행을 위해서는 main 메서드가 있어야 한다.
+	// main 메서드 블록
+	public static void main(String[] args) {
+		// 출력코드
+		System.out.println("Hello, World!");
+	}
+}

0e615c60453f99e8ea8f5f5d499a888d4de5070e
Sangin Kim <tkddls0322@naver.com>
2020-08-05 12:47:58 +0900

    연산자 연습(산술연산)

---
+++ javaEx/src/com/javaex/basic/OperEx.java
@@ -0,0 +1,29 @@
+package com.javaex.basic;
+
+// 연산자 연습
+public class OperEx {
+	public static void main(String[] args) {
+		arithOper();
+	}
+	
+	// 산술연산자
+	public static void arithOper() {
+		// 사칙연산
+		int a = 7;
+		int b = 3;
+		
+		System.out.println("7/3 => "+ (7/3));
+		// int / int -> int(몫)
+		System.out.println("7/3나머지 => "+7%3);
+		
+		// 정확학 실수값을 얻으려면 int -> float 변환
+		System.out.println("7 / 3 실수 => "+((float)a/(float)b));
+		
+		// 만약 -> 정수를 0으로 나누면
+		// ArithmeticException
+		/*
+		 *	System.out.println("정수나누기 0 => "+(a/0));
+		 */
+	}
+
+}
--- javaEx/src/com/javaex/basic/TypeEx.java
+++ javaEx/src/com/javaex/basic/TypeEx.java
@@ -7,8 +7,26 @@ public class TypeEx {
 		floatDouble();
 		booleanEx();
 		charEx();
+		constantEx();
 	}
 	
+	
+	//상수 연습(Constant)
+	public static void constantEx() {
+		// 변하지 않는 데이터
+		// final을 선언부 앞에 붙이면 된다
+		// 상수 식별자는 모두 대문자, 여러 단어 결합시
+		// 단어 사이에 _
+		final double PI = 3.14159;
+		final int SPEED_LIMIT = 110;
+		
+		System.out.println("PI = " + PI);
+		System.out.println("제한속도= "+SPEED_LIMIT);
+		
+//		SPEED_LIMIT = 80; // 상수이므로 변경이 불가하게 된다.
+		System.out.println("제한속도= "+SPEED_LIMIT);
+		
+	}
 	
 	// char 형 연습
 	public static void charEx() {

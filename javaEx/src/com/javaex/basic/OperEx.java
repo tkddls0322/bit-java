567a804575d2a31f4c777713e28360ceb71c6a62
Sangin Kim <tkddls0322@naver.com>
2020-08-06 10:57:06 +0900

    증감, 비교, 논리연산

--- javaEx/src/com/javaex/basic/OperEx.java
+++ javaEx/src/com/javaex/basic/OperEx.java
@@ -4,7 +4,49 @@ package com.javaex.basic;
 public class OperEx {
 	public static void main(String[] args) {
 		arithOper();
+		incDecOper();
+		logicalOper();
 	}
+	
+	// 비교, 논리 연산
+	public static void logicalOper() {
+		System.out.println("----- 비교, 논리 연산 -----");
+		int n = 5;
+		boolean b1 = n > 0;
+		boolean b2 = n < 10;
+		boolean r = b1 && b2;
+		boolean r2 = (n>0)&&(n<10);
+		System.out.println("a is "+n);
+		System.out.println("a > 0 and a < 10 ? "+r2);
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	// 증가, 감소 연산자
+	public static void incDecOper() {
+		System.out.println("----- 증가, 감소 연산자 -----");
+		int a = 5;
+		int b = 5;
+		
+		// 후위 연산자
+		System.out.println(a);
+		System.out.println(a++);
+		System.out.println(a);
+		
+		// 전위 연산자
+		System.out.println(b);
+		System.out.println(++b);
+		System.out.println(b);
+		
+		
+		
+	}
+		
 	
 	// 산술연산자
 	public static void arithOper() {
@@ -24,6 +66,18 @@ public class OperEx {
 		/*
 		 *	System.out.println("정수나누기 0 => "+(a/0));
 		 */
+		// 실수를 0으로 나눈다.
+		System.out.println("실수 / 0 => "+ 4.0/0);
+		// Infinity는 숫자로도 본다
+		System.out.println("무한대의 산술 => "+(4.0/0 - 10000000));
+		// 연산 -> 계산할 수 없는 값(NaN -> Not a Number)
+		System.out.println("0.0 / 0.0 => "+0.0/0.0);
+		// 유한수인지 확인
+		System.out.println("유한수의 확인 : "+Double.isFinite(4.0/0));
+		// NaN인지 확인
+		System.out.println("NaN 확인 => "+Double.isNaN(0.0/0.0));
+		
+		
 	}
 
 }

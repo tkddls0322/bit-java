cb870f9edc1208009e92e3318444322ecab6971e
Sangin Kim <tkddls0322@naver.com>
2020-08-11 11:44:00 +0900

    EnumEx

---
+++ javaEx/src/com/javaex/basic/reftypes/WeekDay.java
@@ -0,0 +1,16 @@
+package com.javaex.basic.reftypes;
+
+// 열거형 장점
+// 오류를 줄일 수 있다
+// 코드 가독성이 상승한다
+
+public enum WeekDay {
+	// 열거 상수: 모두 대문자로
+	SUNDAY,
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY
+}

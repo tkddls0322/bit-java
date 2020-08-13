d622cbf7c4c6bad094c333c14a61609e711e8a08
Sangin Kim <tkddls0322@naver.com>
2020-08-13 10:49:28 +0900

    생성자, 오버로딩 연습

---
+++ javaEx/src/com/javaex/oop/point/v3/PointApp.java
@@ -0,0 +1,25 @@
+package com.javaex.oop.point.v3;
+
+// v3
+public class PointApp {
+
+	public static void main(String[] args) {
+		Point p1 = new Point(); // 기본생성자 사용
+		p1.setX(10);
+		p1.setY(20);
+		
+		Point p2 = new Point(20,30);	// 사용자 생성자 사용
+//		p2.setX(20);
+//		p2.setY(30);
+		
+		p1.draw();
+		p2.draw();
+		
+		// 오버로딩 메서드의 활용
+		p1.draw(false);
+		p2.draw(false);
+		
+		
+	}
+
+}

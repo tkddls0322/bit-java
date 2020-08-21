034bea6772735627a28e8d6a74230b46dcdac4e1
Sangin Kim <tkddls0322@naver.com>
2020-08-21 11:59:40 +0900

    Generics

---
+++ javaEx/src/com/javaex/api/generics/v1/Box.java
@@ -0,0 +1,18 @@
+package com.javaex.api.generics.v1;
+
+//	모든 것들을 담을 수 있는 박스를 설계
+public class Box {
+	Object content;	//	모든 클래스 부모 -> 다 들어감
+
+	//	Getters / Setters
+	public Object getContent() {
+		return content;
+	}
+
+	public void setContent(Object content) {
+		this.content = content;
+	}
+	
+	
+	
+}
